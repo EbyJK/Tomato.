62c46b6942ce68a6092eb8758ec0848489590f72
Eby JK <ebyjkavungal@gmail.com>
2026-04-21 18:36:23 +0530

    checkpoint 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 
 
@@ -7,7 +11,12 @@ const App = () => {
   return (
     <div className='app'>
       <Navbar></Navbar>
-      
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order'element={<PlaceOrder/>}/>
+        
+      </Routes>
     </div>
   )
 }
