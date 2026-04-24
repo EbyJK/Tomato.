a6e6c74f31783950c4d9efdce0be1be1b0b6b055
Eby JK <ebyjkavungal@gmail.com>
2026-04-24 18:41:10 +0530

    checkpoint 8

--- admin/vite-project/src/components/Sidebar/Sidebar.jsx
+++ admin/vite-project/src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './Sidebar.css'
+import { assets } from '../../assets/assets'
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className="sidebar">
+        <div className="sidebar-options">
+             <div className="sidebar-option">
+                <img src={assets.add_icon} alt=""></img>
+                <p>Add Items</p>
+            </div>
+            <div className="sidebar-option">
+                <img src={assets.order_icon} alt=""></img>
+                <p>List Items</p>
+            </div>
+               <div className="sidebar-option">
+                <img src={assets.order_icon} alt=""></img>
+                <p>Orders</p>
+            </div>
+        </div>
       
     </div>
   )
