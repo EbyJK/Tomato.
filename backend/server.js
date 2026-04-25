aa3284b3c3e3fa3828c2e3a76ce79103824f4f24
Eby JK <ebyjkavungal@gmail.com>
2026-04-25 18:37:43 +0530

    checkpoint 9

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from "cors"
 import { connectDB } from "./config/db.js"
 import dotenv from "dotenv";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config.js'
 dotenv.config();
 
 
@@ -22,7 +24,7 @@ connectDB();
 //api endpoints
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
-
+app.use("/api/user",userRouter)
 
 
 app.get("/",(req,res)=>{
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -9,6 +9,8 @@ const StoreContextProvider =(props) => {
 
 
 const [cartItems,setCartItems]=useState({});
+const url="http://localhost:4000"
+const [token,setToken]=useState("");
 
 const addToCart = (itemId)=>{
       if(!cartItems[itemId]){
@@ -49,6 +51,13 @@ const getTotalCartAmount=()=>{
 
 }
 
+useEffect(()=>{
+   if(localStorage.getItem("token")){
+      setToken(localStorage.getItem("token"));
+      
+   }
+
+},[])
 
 
  const contextValue ={
@@ -58,6 +67,9 @@ const getTotalCartAmount=()=>{
     addToCart,
     removeFromCart,
     getTotalCartAmount,
+    url,
+    token,
+    setToken,
 
 
  }
